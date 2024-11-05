e66725b7602b5ff33cd425c33ebf6d594dd55bba
Robert Alexander <robert@robalexdev.com>
2024-11-05 14:02:09 -0500

    Disable service

--- localcert/domains/constants.py
+++ localcert/domains/constants.py
@@ -5,19 +5,20 @@ ACME_CHALLENGE_LABEL = "_acme-challenge"
 API_ENDPOINT_BASE = "https://api.getlocalcert.net/api/v1/acme-dns-compat"
 
 # Abuse limits
-DOMAIN_PER_USER_LIMIT = 5
+DOMAIN_PER_USER_LIMIT = 0 # Disable ahead of shutdown
 DOMAIN_PER_STAFF_LIMIT = 1_000
 
-INSTANT_DOMAINS_PER_HOUR = 100
-INSTANT_DOMAINS_PER_DAY_BURST = 250
-INSTANT_DOMAINS_PER_WEEK = 1000
-DELEGATE_DOMAINS_PER_DAY = 1000
+# Disabled for shutdown
+INSTANT_DOMAINS_PER_HOUR = 0
+INSTANT_DOMAINS_PER_DAY_BURST = 0
+INSTANT_DOMAINS_PER_WEEK = 0
+DELEGATE_DOMAINS_PER_DAY = 0
 
 # To match acme-dns (https://github.com/joohoi/acme-dns/issues/110#issuecomment-826147413)
-TXT_RECORDS_PER_RRSET_LIMIT = 2
+TXT_RECORDS_PER_RRSET_LIMIT = 0 # Disabled for shutdown
 
 # To match AWS AK/SK approach, keep two active to rotate keys
-API_KEY_PER_ZONE_LIMIT = 2
+API_KEY_PER_ZONE_LIMIT = 0 # Disabled for shutdown
 
 # Default Email Security Policy
 #
